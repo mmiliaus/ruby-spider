c2f3fb3c69eb526839999417c6504b51057e261c
Martynas Miliauskas <mmiliauskass@gmail.com>
2013-03-23 16:31:41 +0000

    step 13. namespaces

--- spider.rb
+++ spider.rb
@@ -2,67 +2,70 @@ require 'open-uri'
 
 WIKIPEDIA_DOMAIN = 'http://en.wikipedia.org'
 
-class Page
+module Wikipedia
 
-  attr_accessor :heading, :abstract, :links
+  class Page
 
-  def initialize resource
-    @resource = resource
-  end
+    attr_accessor :heading, :abstract, :links
 
-  def download
-    url = "#{WIKIPEDIA_DOMAIN}/wiki/#{@resource}"
-    @source = open(url).read
-    self
-  end
-
-  def get_data
-    @heading = get_heading(@source)
-    @abstract = get_abstract(@source)
-    @links = get_links(@source)
-  end
-
-  def get_heading source_html
-    m = source_html.match(/<h1.*>(.+?)<\/h1>/im)
-    m[1].gsub(/<\/?.+>/, "")
-  end
-
-  def get_abstract source_html
-    m = source_html.match /\<\/table>\s+(<p>.+)<table.+?id="toc"/im
-    m[1]
-  end
-
-  def get_links source_html
-    source_html.scan(/<a.+?href="(.+?)"/im).flatten
-  end
-
-  def self.filter_links filters, links
-    filtered_links = links
-    if filters.include? :hash_tags
-      filtered_links = self.filter_hash_tags filtered_links
+    def initialize resource
+      @resource = resource
     end
 
-    if filters.include? :double_slashes
-      filtered_links = self.filter_double_slashes filtered_links
+    def download
+      url = "#{WIKIPEDIA_DOMAIN}/wiki/#{@resource}"
+      @source = open(url).read
+      self
     end
 
-    if block_given?
-      filtered_links = filtered_links.select do |url| 
-        not yield(url)
+    def get_data
+      @heading = get_heading(@source)
+      @abstract = get_abstract(@source)
+      @links = get_links(@source)
+    end
+
+    def get_heading source_html
+      m = source_html.match(/<h1.*>(.+?)<\/h1>/im)
+      m[1].gsub(/<\/?.+>/, "")
+    end
+
+    def get_abstract source_html
+      m = source_html.match /\<\/table>\s+(<p>.+)<table.+?id="toc"/im
+      m[1]
+    end
+
+    def get_links source_html
+      source_html.scan(/<a.+?href="(.+?)"/im).flatten
+    end
+
+    def self.filter_links filters, links
+      filtered_links = links
+      if filters.include? :hash_tags
+        filtered_links = self.filter_hash_tags filtered_links
       end
+
+      if filters.include? :double_slashes
+        filtered_links = self.filter_double_slashes filtered_links
+      end
+
+      if block_given?
+        filtered_links = filtered_links.select do |url| 
+          not yield(url)
+        end
+      end
+
+      filtered_links
+    end
+    
+    def self.filter_hash_tags links
+      links.select{|url| not url.match(/\A#/)}
     end
 
-    filtered_links
-  end
-  
-  def self.filter_hash_tags links
-    links.select{|url| not url.match(/\A#/)}
-  end
+    def self.filter_double_slashes links
+      links.select{|url| not url.match(/\/\//)}
+    end
 
-  def self.filter_double_slashes links
-    links.select{|url| not url.match(/\/\//)}
   end
-
 end
 
 # example ARGV: ["-r", "Ruby_on_Rails", "-s", "links"]
@@ -85,11 +88,7 @@ if !params.include?(:r)
 end
 
 
-wp = Page.new params[:r]
+wp = Wikipedia::Page.new params[:r]
 wp.download.get_data
 
-f_links = Page.filter_links(
-                [:hash_tags, :double_slashes], 
-                wp.links
-          ) { |url| url.match(/wiki\/.+:.+/) }
-puts f_links
+puts wp.heading
